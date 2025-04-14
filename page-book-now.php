d8dd9702aded74f8b0ce507b9c8a6b05d8adfcc7
ramGit22 <karki_ram@hotmail.com>
2025-04-14 08:02:52 +0300

    initial commit

---
+++ page-book-now.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ Template Name: Book Now
+*/
+get_header(); ?>
+<main class="booking-page">
+    <h2>Schedule Your Repair</h2>
+    <div class="booking-form">
+        <?php the_content(); ?>
+    </div>
+</main>
+<?php get_footer(); ?>
