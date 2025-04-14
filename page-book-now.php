8675c87e187e643a9c3f678ea3e557355860d9c0
ramGit22 <karki_ram@hotmail.com>
2025-04-14 16:04:56 +0300

    Remove service details from booking page for simplified layout

--- page-book-now.php
+++ page-book-now.php
@@ -12,55 +12,7 @@ get_header(); ?>
                 <p><?php echo esc_html__('Varaa autohuoltoaikasi verkossa. Valitse ammattitaitoisista palveluistamme.', 'car-repair-theme'); ?></p>
             </div>
             
-            <div class="booking-form">
-                <div class="service-info">
-                    <h3><?php echo esc_html__('Palvelumme', 'car-repair-theme'); ?></h3>
-                    <div class="service-list">
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-oil-can"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Öljynvaihto', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Säännölliset öljynvaihdot pitävät moottorisi kunnossa', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-cog"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Renkaiden vaihto + kausisäilytys', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Renkaanvaihto- ja kausisäilytyspalvelut', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-car"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Määräaikaishuolto', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Säännöllinen huolto ajoneuvosi kilometrilukeman perusteella', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-snowflake"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Ilmastointihuolto', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Ilmastointijärjestelmän huolto- ja korjauspalvelut', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-balance-scale"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Nelipyöräsuuntaus', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Pyöränsuuntauspalvelut tasaisempaa ajoa varten', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                        <div class="service-item">
-                            <div class="service-icon"><i class="fas fa-lightbulb"></i></div>
-                            <div class="service-text">
-                                <h4><?php echo esc_html__('Lampunvaihto', 'car-repair-theme'); ?></h4>
-                                <p><?php echo esc_html__('Nopea polttimonvaihtopalvelu', 'car-repair-theme'); ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
+            <div class="booking-form">                
                 <div class="booking-system">
                     <?php the_content(); ?>
                 </div>
